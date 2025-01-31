a06874cf33b490d9131415796e6b7c7a50e201f9
ThracusRO <samuel.trif@gmail.com>
2025-01-31 22:17:26 +0200

    initial push

--- Models/Specializare.cs
+++ Models/Specializare.cs
@@ -8,6 +8,6 @@ namespace Sala_Fitness_Trif_Samuel.Models
 
         [Required(ErrorMessage = "Denumirea este obligatorie.")]
         [StringLength(30, ErrorMessage = "Denumirea nu poate avea mai mult de 30 de caractere.")]
-        public string Denumire { get; set; }  // Denumirea specializarii (Personal Trainer, Yoga, etc.)
+        public string Denumire { get; set; }  // Denumirea specializarii (Fitness, Yoga, Inot etc.)
     }
 }
